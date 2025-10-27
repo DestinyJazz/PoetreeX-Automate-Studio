2e73d7ea2dc8d7a66d96e4cc71dcd576d576581a
LaiCheeHong <gerrardhong8@gmail.com>
2025-10-27 11:07:50 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // if using React
 
 export default defineConfig({
+  base: '/PoetreeX_Automate_Studio/', // Add this line!
   plugins: [react()],
-  base: '/PoetreeX-Automate-Studio/'
 })
